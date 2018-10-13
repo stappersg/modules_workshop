47b7c094cf2d7f0bb778fe533650a1ce9ff42a54
Geert Stappers <stappers@stappers.nl>
2018-10-13 11:19:11 +0200

    demo.py:4:80: E501 line too long (92 > 79 characters)

--- library/demo.py
+++ library/demo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 # (c) 2017, Joris Weijters <joris.weijters@gmail.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
